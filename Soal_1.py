0085ff86e3b3a1d1cf11cc58f6f3e423934dd3c4
Tesadiansaputri <tesadiansaputri@gmail.com>
2024-11-13 12:03:31 +0700

    Add files via upload

---
+++ Soal_1.py
@@ -0,0 +1,8 @@
+a = 25
+b = 10
+hasil_1 = a + b
+hasil_2 = a - b
+hasil_3 = a * b
+print(hasil_1)
+print(hasil_2)
+print(hasil_3)
---
+++ Soal_3.py
@@ -0,0 +1,12 @@
+jumlah_barang = input("Masukan jumlah barang: ")
+barang1 = float(input("Masukan harga barang1: "))
+barang2 = float(input("Masukan harga barang2: "))
+barang3 = float(input("Masukan harga barang3: "))
+
+total_belanja = barang1 + barang2 + barang3
+
+print(jumlah_barang)
+print(barang1)
+print(barang2)
+print(barang3)
+print(total_belanja)
